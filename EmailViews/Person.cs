56c5140aa83a971e795c7146d0f60fcc7a9b35a5
Jonathan Smart <JonathanSmart@ChessICT.co.uk>
2018-09-30 16:51:51 +0100

    Created

---
+++ EmailViews/Person.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace EmailViews
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public List<string> PurchasedItems { get; set; }
+    }
+}
---
+++ EmailViews/Program.cs
@@ -0,0 +1,27 @@
+﻿using RazorEngine.Templating;
+using System;
+using System.IO;
+using System.Net.Mail;
+
+namespace EmailViews
+{
+    class Program
+    {
+        static readonly string TemplateFolderPath =
+         Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "EmailTemplates");
+
+        static void Main(string[] args)
+        {
+            var model = new Person() { Name = "Joe Doe" };
+
+            var emailTemplatePath = Path.Combine(TemplateFolderPath, "InviteEmailTemplate.cshtml");
+            var templateService = new TemplateService();
+            var emailHtmlBody = templateService.Parse(File.ReadAllText(emailTemplatePath), model, null, null);
+
+            Console.WriteLine(emailHtmlBody);
+
+            Console.ReadLine();
+
+        }
+    }
+}
